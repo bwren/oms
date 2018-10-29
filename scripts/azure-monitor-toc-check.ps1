8e6a5e2c115300f6a7d209ec6a86f4d397cfc28d
Brian Wren <bwren@microsoft.com>
2018-10-29 15:51:30 -0700

    azure monitor scripts

---
+++ scripts/azure-monitor-toc-check.ps1
@@ -0,0 +1,14 @@
+﻿$articles = get-childitem -path C:\Users\bwren\Git\azure-docs-pr\articles\azure-monitor\*.md -Recurse
+$articles += get-childitem -path C:\Users\bwren\Git\azure-docs-pr\articles\monitoring\*.md -Recurse
+$articles += get-childitem -path C:\Users\bwren\Git\azure-docs-pr\articles\log-analytics\*.md -Recurse
+$articles += get-childitem -path C:\Users\bwren\Git\azure-docs-pr\articles\application-insights\*.md -Recurse
+$articles += get-childitem -path C:\Users\bwren\Git\azure-docs-pr\articles\monitoring-and-diagnostics\*.md -Recurse
+
+$toc = gc C:\Users\bwren\Git\azure-docs-pr\articles\azure-monitor\toc.yml
+
+foreach ($article in $articles)
+{
+    
+    if (($toc | Select-String $article.Name -Quiet) -ne $true) {$article.Name}
+
+}
